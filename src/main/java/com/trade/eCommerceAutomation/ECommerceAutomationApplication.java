4a01d1d96377c5a899792aeb462ec1322109cae0
studentAybu <23050161004@aybu.edu.tr>
2025-01-21 01:26:28 +0300

    first commit

---
+++ src/main/java/com/trade/eCommerceAutomation/ECommerceAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.trade.eCommerceAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trade/eCommerceAutomation/ECommerceAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade.eCommerceAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
